a14502cb288066953c8f5ac27085bfac3a101951
AndriyZhabynets <96949523+AndriyZhabynets@users.noreply.github.com>
2024-06-19 02:04:37 +0300

    Changes

--- Диплом/backend-restful-api-main/src/server.js
+++ Диплом/backend-restful-api-main/src/server.js
@@ -1,7 +1,51 @@
+// 'use strict';
+
+// const express = require('express');
+// const cors = require('cors');
+// require('dotenv/config');
+// const ordersRouter = require('./ordersAPI/ordersRouter');
+// const phonesRouter = require('./phonesAPI/phonesRouter');
+// const usersRouter = require('./usersAPI/userRouter');
+
+// const app = express();
+// const router = express.Router();
+// const PORT = process.env.PORT || 8000;
+
+// app.use(cors({
+//     origin: [process.env.CLIENT_DEV_URL, process.env.CLIENT_PROD_URL],
+//     credentials: true,
+// }));
+// app.use(express.json());
+// app.use(router);
+
+// // Home page
+// router.get('/', (request, response) => {
+//     try {
+//         response.send(
+//           'Welcome to Nice-Gadgets Back-End SERVER!'
+//           + '<hr><br /> Visit us here - https://github.com/AndriyZhabynets/graduate-work.github.io'
+//         );
+//     } catch (e) {
+//         response.sendStatus(500);
+//     }
+// });
+
+// // Products/phones
+// app.use('/products', phonesRouter);
+
+// // Order:
+// app.use('/orders', ordersRouter);
+
+// // Auth/Login/Register users:
+// app.use('/users', usersRouter)
+
+// // server init
+// app.listen(PORT, () => {
+//     console.log(`running on - http://localhost:${PORT}`);
+// });
 'use strict';
 
 const express = require('express');
-const cors = require('cors');
 require('dotenv/config');
 const ordersRouter = require('./ordersAPI/ordersRouter');
 const phonesRouter = require('./phonesAPI/phonesRouter');
@@ -11,33 +55,34 @@ const app = express();
 const router = express.Router();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors({
-    origin: [process.env.CLIENT_DEV_URL, process.env.CLIENT_PROD_URL],
-    credentials: true,
-}));
+// Налаштування CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_PROD_URL);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 app.use(express.json());
 app.use(router);
+
+// Обробка запитів для роутерів products, orders, users
+app.use('/products', phonesRouter);
+app.use('/orders', ordersRouter);
+app.use('/users', usersRouter);
 
 // Home page
 router.get('/', (request, response) => {
     try {
         response.send(
           'Welcome to Nice-Gadgets Back-End SERVER!'
-          + '<hr><br /> Visit us here - https://fe-aug22-team-harold.github.io/nice_gadgets_FE'
+          + '<hr><br /> Visit us here - https://github.com/AndriyZhabynets/graduate-work.github.io'
         );
     } catch (e) {
         response.sendStatus(500);
     }
 });
-
-// Products/phones
-app.use('/products', phonesRouter);
-
-// Order:
-app.use('/orders', ordersRouter);
-
-// Auth/Login/Register users:
-app.use('/users', usersRouter)
 
 // server init
 app.listen(PORT, () => {
